3de66534c6681a2f806f74ce709a78f9f319f80e
gpk659 <pyck.greg@hotmail.fr>
2016-11-16 17:07:46 +0100

    Classe Joueur et Classe Main

---
+++ src/Joueur.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+
+/**
+ * @author Grégory
+ *
+ */
+public class Joueur {
+
+	public String nom;
+	public String prenom;
+	
+	public int vie;
+	public int etat;
+	
+	private String motFinal;
+	private String motEnCours;
+	
+	
+	// getters & setters pour y acceder depuis les autres classes
+	private String getNom() {
+		return nom;
+	}
+	private void setNom(String nom) {
+		this.nom = nom;
+	}
+	private String getPrenom() {
+		return prenom;
+	}
+	private void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	private int getVie() {
+		return vie;
+	}
+	private void setVie(int vie) {
+		this.vie = vie;
+	}
+	private int getEtat() {
+		return etat;
+	}
+	private void setEtat(int etat) {
+		this.etat = etat;
+	}
+	
+	
+
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author Grégory
+ *
+ */
+public class Main {
+
+	/**
+	 * classe main
+	 * notre classe principale
+	 * c'est ici que tout ce lancera 
+	 *
+	 */
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
