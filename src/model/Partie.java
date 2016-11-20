d83db1038c72b7fbaf4db987a43b53ae8c211104
simonbarre <simonbarre@users.noreply.github.com>
2016-11-20 17:33:58 +0100

    Update Partie.java

--- src/model/Partie.java
+++ src/model/Partie.java
@@ -1,31 +1,52 @@
-/**
- * 
- */
 package model;
 
+import java.util.Scanner;
+
 /**
- * @author Grégory
+ * @author Gregory Pyck & Simon Barre
  *
  */
 public class Partie {
 
-	int nbJoueur;
-	Mot mot;
-	int joueurActif;
-	int [] point;
+	private int nbJoueur;
+	protected Mot mot;
+	protected int joueurActif;
+	protected int point[] = {30,20,15,10,5,1,0};
+	private Scanner sc = new Scanner(System.in);
 	
 	
 	public void definirJoueurActif(){
 		
 	}
-	public Joueur creerJoueur(){
+	
+	public void creerJoueur(){
+		/*for (int i = 0; i<nbJoueur;){
+			i++;
+			System.out.println("Quel est votre nom ? ");
+			String strNom = sc.nextLine();
+			
+			System.out.println("Quel est votre prenom ? ");
+			String strPrenom = sc.nextLine();
+			
+			String nomJoueur = "joueur" + i;
+			
+			Joueur nomJoueur = new Joueur(strNom,strPrenom);
+			
+		}*/
 		
-		return null;
+		System.out.println("Quel est votre nom ? ");
+		String strNom = sc.nextLine();
 		
+		System.out.println("Quel est votre prenom ? ");
+		String strPrenom = sc.nextLine();
+		
+		Joueur j1 = new Joueur(strNom,strPrenom);
 	}
+	
 	public void updateScore(){
 		
 	}
+	
 	public void affichScore(){
 		
 	}
