0dec83f5eba25fd0384436e641179ce78b61e3f2
simonbarre <simonbarre@users.noreply.github.com>
2016-11-25 11:08:49 +0100

    Update Partie.java

--- src/model/Partie.java
+++ src/model/Partie.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class Partie {
 
-	private Joueur tabNbJoueur[]; //= new Joueur[1];
+	protected Joueur tabNbJoueur[]; //= new Joueur[1];
 	//protected Mot mot;
 	//protected int joueurActif;
 	protected int point[] = {30,20,15,10,5,1,0};
@@ -22,6 +22,11 @@ public class Partie {
 		creerJoueur(sc.nextInt());
 		// + Creer mot, mais pas encore fini (au 25/11/16)
 	}
+	
+	public Partie(int nbJ, String pseudo) {
+		creerJoueur2(nbJ, pseudo);
+		// + Creer mot, mais pas encore fini (au 25/11/16)
+	}
 
 	public void creerJoueur(int nbJ){
 		tabNbJoueur = new Joueur[nbJ];
@@ -31,6 +36,16 @@ public class Partie {
 			i++;
 		}
 	}
+	
+	public void creerJoueur2(int nbJ, String pseudo){
+		tabNbJoueur = new Joueur[nbJ];
+		int i=0;
+		while (i<nbJ){
+			tabNbJoueur[i] = new Joueur(pseudo);
+			i++;
+		}
+	}
+	
 	
 	public void updateScore(){
 		
@@ -49,7 +64,10 @@ public class Partie {
 		    if (getClass() != obj.getClass())
 		        return false;
 		    Partie other = (Partie) obj;
-		    
+		    if (other.tabNbJoueur.length==0 && this.tabNbJoueur.length!=0)
+		    	return false;
+		    if (other.tabNbJoueur.length != this.tabNbJoueur.length)
+		    	return false;
 		   return true;
 		}
 }
