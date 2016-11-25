5e23d534e3d520040de2cd68caab3ee7bc55c537
simonbarre <simonbarre@users.noreply.github.com>
2016-11-25 10:32:45 +0100

    Update Partie.java

--- src/model/Partie.java
+++ src/model/Partie.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 
 /**
  * @author Gregory Pyck & Simon Barre 2TL1 G4
- *
  */
 public class Partie {
 
-	private int nbJoueur;
+	private Joueur tabNbJoueur[]; //= new Joueur[1];
 	//protected Mot mot;
-	protected int joueurActif;
+	//protected int joueurActif;
 	protected int point[] = {30,20,15,10,5,1,0};
 	private Scanner sc = new Scanner(System.in);
-	
 	
 	public void definirJoueurActif(){
 		
@@ -21,15 +19,15 @@ public class Partie {
 	
 	public Partie() {
 		System.out.println("Conbien de joueur ?");
-		nbJoueur = sc.nextInt();
-		creerJoueur();
+		creerJoueur(sc.nextInt());
+		// + Creer mot, mais pas encore fini (au 25/11/16)
 	}
 
-	public void creerJoueur(){
-		Joueur tabNJ[] = new Joueur[nbJoueur];
+	public void creerJoueur(int nbJ){
+		tabNbJoueur = new Joueur[nbJ];
 		int i=0;
-		while (i<nbJoueur){
-			tabNJ[i]= new Joueur();
+		while (i<nbJ){
+			tabNbJoueur[i] = new Joueur();
 			i++;
 		}
 	}
@@ -41,4 +39,17 @@ public class Partie {
 	public void affichScore(){
 		
 	}
+	
+	//methode equals
+		public boolean equals(Object obj){
+		    if (this == obj)
+		        return true;
+		    if (obj == null)
+		        return false;
+		    if (getClass() != obj.getClass())
+		        return false;
+		    Partie other = (Partie) obj;
+		    
+		   return true;
+		}
 }
