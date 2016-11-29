b770eefa1e53845cb136ca5d33acaf47c970d0ca
bari <pyck.greg@hotmail.fr>
2016-11-29 09:38:01 +0100

    code final pour la classe mot

--- src/model/Mot.java
+++ src/model/Mot.java
@@ -1,48 +1,76 @@
-/**
- * 
- */
-package modèle;
-
-import java.nio.file.Files;
+package model;
 
 /**
- * @author Amine
- *
+ * @author Grégory
  */
-public class Mot {
 
-		private Files dico;
-		private String motChoisi;
-		private int nbLettres;
-		private int error = 0;
-		
-		/*
-	     * Constructeurs 
-	     */
-		
-		public Mot(String v, int x) {
-			motChoisi = v;
-			nbLettres = x;
-		}
-		
-		/*
-	     * Getters and Setters 
-	     */
-		public void choisirMot(Files dico) {
-			this.dico = dico;
-		}
-		public String getMotChoisi() {
-			return motChoisi;
-		}
-		public void setMotChoisi(String motChoisi) {
-			this.motChoisi = motChoisi;
-		}
-		public int getNbLettres() {
-			return nbLettres;
-			}
-		public void setNbLettres(int nbLettres) {
-			this.nbLettres = nbLettres;
-		}
-		
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
 
+import javax.swing.JOptionPane;
+
+public class Mot{
+
+	private String word = "", secretMot = "";
+	private char[] tabChar;
+	private int error = 0;
+	private int nbreCoup = 0;
+
+	public Mot(){
+		initWord();
+		System.out.println(word);
+		System.out.println(secretMot);
 	}
+	
+	public void initWord(){
+		
+		int i = (int)(Math.random() * 14); //14 pour 14 lignes dans le fichier dictionnaire
+		while(i > 14){
+			 i /= 2;
+		}
+		
+		try {//ouverture du fichier
+			LineNumberReader fnr = new LineNumberReader(new FileReader(new File("file/dictionnaire.txt")));
+			int carac;
+			this.word = "";
+			this.secretMot= "";
+			while((carac = fnr.read()) != -1){
+				if(fnr.getLineNumber() == (i+1))
+					break;
+				
+				else{
+					if(fnr.getLineNumber() == i){
+						this.word += (char)carac;						
+					}
+				}
+			}
+			
+			/**
+			 * trim() : est utilisé pour supprimer les espaces au début et à la fin du mot
+			 * toUpperCase() : convertit tous les caractères de la chaîne en majuscules
+			 */
+			this.word = this.word.trim().toUpperCase();
+			
+			for(int j = 0; j < this.word.length(); j++)
+			{//	on injecte une * pour chaque caractère en parcourant le mot avec charAt
+				this.secretMot += (this.word.charAt(j) != '\'' && this.word.charAt(j) != '-') ? "*" : this.word.charAt(j);
+			}
+			
+			fnr.close();//on ferme l'ouverture du fichier
+			this.nbreCoup = 0;
+		} catch (FileNotFoundException e) {//gestions des erreurs si l'ouverture du fichier n'est pas réussie
+			JOptionPane.showMessageDialog(null, "Erreur de chargement depuis le fichier de mots !", "ERREUR", JOptionPane.ERROR_MESSAGE);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Erreur de chargement depuis le fichier de mots !", "ERREUR", JOptionPane.ERROR_MESSAGE);
+		}
+		
+		/**
+		 * toCharArray : convertit les * en tableau de charactère
+		 */
+		this.tabChar = this.secretMot.toCharArray();
+		this.error = 0;
+	}
+}
