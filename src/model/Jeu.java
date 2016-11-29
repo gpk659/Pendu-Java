a0be309ebe2940855ba190f297ca350f24d87083
Greg <pyck.greg@hotmail.fr>
2016-11-29 10:43:13 +0100

    Update Jeu.java

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -1,15 +1,11 @@
 package model;
-
 /**
  * @author Grégory Pyck & Simon Barré
- *
  */
 public class Jeu {
-
 	int nbEssai;
 	
-	
 	public static void main(String[] args) {
-		Partie p0 = new Partie();
+		Partie p1 = new Partie();
 	}
 }
