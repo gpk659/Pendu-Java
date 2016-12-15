833705e656935d7c3e5c32f52a26b3798ef1ef94
Aminenaf <amine.naf@hotmail.com>
2016-12-15 10:10:26 +0100

    Update Accueil.java

--- src/view/Accueil.java
+++ src/view/Accueil.java
@@ -11,7 +11,11 @@ public class Accueil extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
 	public Accueil() {
-		JLabel titre = new JLabel("Bienvenue dans le jeu du pendu\n");
-		titre.setHorizontalAlignment(JLabel.CENTER);
+		JLabel titre = new JLabel("Bienvenue, voici le jeu du Pendu !");
+		JButton button = new JButton("PLAY");
+		JLabel image = new JLabel(new ImageIcon("images/dessin-pendu-1.png"));
+		add(image);
+		add(titre);
+		add(button);
 	}
 }
